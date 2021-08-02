2ee1f2a163bf0eb860c967ac4b25ced7f5ca557a
Eduard4002 <54219870+Eduard4002@users.noreply.github.com>
2021-08-02 22:26:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CollatzProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goAgain = true;
+            while(goAgain){
+                bool isOne = false;
+                long numberToStart = 0, count = 0;
+                Console.Write("Enter a number to start with: ");
+                numberToStart = long.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                long currentNumber = numberToStart;
+                while(isOne == false){
+                    //To make sure we dont get stuck in a infinitive loop
+                    if(currentNumber == 1) isOne = true;
+                    Console.WriteLine($"Current Number: " + currentNumber);
+                    //number is even, divide by 2
+                    if(currentNumber % 2 == 0){
+                        currentNumber /= 2;
+                        //number is odd, multiply by 3 and add 1
+                    }else if(currentNumber % 2 != 0){
+                        currentNumber = 3 * currentNumber + 1;
+                    }
+                    //increase count
+                    count++;
+                }
+                Console.WriteLine($"Process finished : Went through {count} counts");
+                Console.WriteLine("Would you like to go again? [Y:N]");
+                string temp = Console.ReadLine();
+                Console.WriteLine("");
+                if(temp == "N" || temp == "n"){
+                    goAgain = false;
+                }
+            
+            }
+            return;
+        }
+    }
+}
+            
+    
+
